02f0117e26dcf9fea0c845a509a0267683cb3cbe
Davide Casale <davi0kprogramsthings@gmail.com>
2025-04-17 00:54:17 +0200

    Use esm for all imports (+ append license to each file).

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Copyright 2024 Tether Operations Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict'
+
 /**
  * @fileoverview Wallet service for TON blockchain using mnemonic and key pairs
  */
@@ -6,14 +21,20 @@ import {
   mnemonicNew, 
   mnemonicValidate, 
   mnemonicToSeed,
+  mnemonicToPrivateKey,
   keyPairFromSeed, 
   keyPairFromSecretKey, 
   sign, 
-  signVerify, 
-  KeyPair 
+  signVerify
 } from '@ton/crypto';
-const { WalletContractV5R1 } = require('@ton/ton');
+
+import { WalletContractV5R1 } from "@ton/ton";
+
 import nacl from 'tweetnacl';
+
+/**
+ * @typedef {import('@ton/crypto').KeyPair} KeyPair
+ */
 
 /**
  * Service class for managing TON wallets
