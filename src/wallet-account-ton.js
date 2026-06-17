7eaf6e56e5c3a9bbc077ac0ddcdba89c6472c902
Boka44 <nicholashrboka@gmail.com>
2026-06-17 15:37:10 +0200

    added throw docs

--- src/wallet-account-ton.js
+++ src/wallet-account-ton.js
@@ -145,6 +145,7 @@ export default class WalletAccountTon extends WalletAccountReadOnlyTon {
    *
    * @param {TonTransaction} tx - The transaction to sign.
    * @returns {Promise<Cell>} The signed external-message body as a TON Cell.
+   * @throws {Error} If the transaction's cost exceeds the maximum transaction fee option.
    */
   async signTransaction (tx) {
     if (!this._tonClient) {
@@ -170,6 +171,7 @@ export default class WalletAccountTon extends WalletAccountReadOnlyTon {
    *
    * @param {TonTransaction} tx - The transaction.
    * @returns {Promise<TransactionResult>} The transaction's result.
+   * @throws {Error} If the transaction's cost exceeds the maximum transaction fee option.
    */
   async sendTransaction (tx) {
     if (!this._tonClient) {
@@ -198,6 +200,7 @@ export default class WalletAccountTon extends WalletAccountReadOnlyTon {
    *
    * @param {TransferOptions} options - The transfer's options.
    * @returns {Promise<TransferResult>} The transfer's result.
+   * @throws {Error} If the transfer's cost exceeds the maximum transfer fee option.
    */
 
   async transfer (options) {
